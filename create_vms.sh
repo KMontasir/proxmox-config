772b2102fca6b19f82d300a61c34329b5aafbcb6
Montasir <161086149+KMontasir@users.noreply.github.com>
2025-01-08 15:40:59 +0100

    Update create_vms.sh

--- create_vms.sh
+++ create_vms.sh
@@ -2,61 +2,48 @@
 
 # Fonction pour créer les VMs avec configuration matérielle complète
 create_vms() {
+    VM_STORAGE="local-lvm-thin"
 
-    # Récupérer dynamiquement le stockage cloud-init
-    CLOUD_INIT_STORAGE=""
-    for storage_name in "${!STORAGE_CONFIGS[@]}"; do
-        if [[ "$storage_name" == "cloud-init" ]]; then
-            CLOUD_INIT_STORAGE="$storage_name"
-            break
-        fi
-    done
-
-    if [[ -z "$CLOUD_INIT_STORAGE" ]]; then
-        echo "Erreur : le stockage 'cloud-init' n'est pas défini dans STORAGE_CONFIGS."
-        exit 1
-    fi
-
-    # Création des groupes de ressources
     echo "Création des groupes de ressources..."
-    for pool in pare-Feu zone-relais zone-exposee service-interne template labo; do
-        pveum pool add "$pool"
+    for pool in Pare-Feu zone-relais zone-exposee service-interne template; do
+        pveum pool add $pool
     done
 
-    # Création de la VM Firewall Template
-    VM_NAME="PfSenseTemplate"
-    DISK_SIZE=${DISK_CONFIGS["$VM_NAME"]}
-    echo "Création du volume LVM pour $VM_NAME avec taille $DISK_SIZE sur $CLOUD_INIT_STORAGE..."
-    lvcreate --thinpool "$CLOUD_INIT_STORAGE/thinpool" --virtualsize "$DISK_SIZE" -n vm-9999-disk-0
+    # Créez un volume LVM pour la VM Firewall
+    echo "Création du volume LVM pour Firewall..."
+    lvcreate --thinpool $VM_STORAGE/thinpool --virtualsize 16G -n vm-9999-disk-0
 
     echo "Création de la VM Firewall..."
     qm create 9999 \
-        --name "FirewallTemplate" \
+        --name "Firewall" \
         --memory 2048 \
         --cores 2 \
         --net0 "virtio,bridge=vmbr0" \
         --net1 "virtio,bridge=vmbr1" \
-        --cdrom "$OPNSENSE_ISO" \
+        --cdrom $OPNSENSE_ISO \
         --boot c \
         --bootdisk scsi0 \
-        --scsi0 "$CLOUD_INIT_STORAGE:vm-9999-disk-0" \
+        --scsi0 $VM_STORAGE:vm-9999-disk-0 \
         --pool template
 
-    # Création de la VM serveur Web Template
-    VM_NAME="WebServerTemplate"
-    DISK_SIZE=${DISK_CONFIGS["$VM_NAME"]}
-    echo "Création du volume LVM pour $VM_NAME avec taille $DISK_SIZE sur $CLOUD_INIT_STORAGE..."
-    lvcreate --thinpool "$CLOUD_INIT_STORAGE/thinpool" --virtualsize "$DISK_SIZE" -n vm-9001-disk-0
+    # Créez un volume LVM pour le serveur Web
+    echo "Création du volume LVM pour WebServerTemplate..."
+    lvcreate --thinpool $VM_STORAGE/thinpool --virtualsize 32G -n vm-9001-disk-0
 
     echo "Création du serveur web template..."
-    qm create 9989 \
+    qm create 9001 \
         --name "WebServerTemplate" \
         --memory 2048 \
         --cores 2 \
         --net0 "virtio,bridge=vmbr1,tag=20" \
-        --cdrom "$DEBIAN_ISO" \
+        --cdrom $LINUX_ISO \
         --boot c \
         --bootdisk scsi0 \
-        --scsi0 "$CLOUD_INIT_STORAGE:vm-9001-disk-0" \
+        --scsi0 $VM_STORAGE:vm-9001-disk-0 \
         --pool template
+
+    echo "Démarrage des VMs..."
+    #for vm in 9999 8999 9001; do
+    #    qm start $vm
+    #done
 }
