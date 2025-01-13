d64f80fc084dabf618f8ef273955f6af34ca3542
Montasir <161086149+KMontasir@users.noreply.github.com>
2025-01-13 09:40:27 +0100

    Update setup_appliance.sh

--- setup_appliance.sh
+++ setup_appliance.sh
@@ -5,12 +5,12 @@ setup_appliance() {
     # Installer et configurer OpenSSH
     echo "Installation et configuration d'OpenSSH'..."
     apt install openssh-server -y
-	sed -i 's/^#Port 22/Port 2222/' /etc/ssh/sshd_config
-	sed -i 's/^PermitRootLogin .*/PermitRootLogin no/' /etc/ssh/sshd_config
+    sed -i 's/^#Port 22/Port 2222/' /etc/ssh/sshd_config
+    sed -i 's/^PermitRootLogin .*/PermitRootLogin no/' /etc/ssh/sshd_config
     sed -i 's/^#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
     sed -i 's/^#AuthorizedKeysFile .*/AuthorizedKeysFile     .ssh/authorized_keys .ssh/authorized_keys2/' /etc/ssh/sshd_config
     sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-	systemctl restart sshd
+    systemctl restart sshd
     echo "OpenSSH configuré avec succès."
 
     # Installer et configurer Open vSwitch
