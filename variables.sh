9a9ee7261128a280c23d5d01d6805fa06876bdf4
Montasir <161086149+KMontasir@users.noreply.github.com>
2025-01-13 09:23:44 +0100

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -2,13 +2,13 @@
 
 # Variables utilisateurs
 declare -A PROXMOX_USERS=( 
-    ["marcus"]="pveadmin-marcus@pve" 
-    ["jeanbatiste"]="pveadmin-jeanbatiste@pve"
-    ["philippe"]="pveadmin-philippe@pve"
-    ["montasir"]="pveadmin-montasir@pve"
+    ["user1"]="pveadmin-user1@pve" 
+    ["user2"]="pveadmin-user2@pve"
+    ["user3"]="pveadmin-user3@pve"
+    ["user4"]="pveadmin-user4@pve"
 )
-PROXMOX_PASSWORD="Azerty/123"
-PVE_PASSWORD="Azerty/123"
+PROXMOX_PASSWORD=""
+PVE_PASSWORD=""
 
 # Variables fichiers ISO
 OPNSENSE_ISO=""
@@ -16,15 +16,15 @@ DEBIAN_ISO=""
 
 # Variables Stockage
 declare -A STORAGE_CONFIGS=(
-    ["local-lvm-thin"]="/dev/sdb"
-    ["cloud-init"]="/dev/sdc"
+    ["local-lvm-template"]="/dev/sdb"
+    ["local-lvm-vm"]="/dev/sdc"
 )
 
 # Variables Disque pour les VMs
 declare -A DISK_CONFIGS=(
-    ["PfSenseTemplate"]="16G"
+    ["OpnsenseTemplate"]="16G"
     ["WebServerTemplate"]="16G"
 )
 
 # Variable Interface physique bridged sur VLAN10
-NETWORK_INTERFACE="ens34"
+NETWORK_INTERFACE="eno2"
